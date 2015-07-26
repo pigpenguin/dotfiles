09d286c88f878fdda18fa455a1c7bbc9b3abe175
pigpenguin <ethan@rooke.org>
2015-07-26 09:47:19 -0700

    fixed some colors

--- herbstluftwm/panel.sh
+++ herbstluftwm/panel.sh
@@ -1,3 +1,4 @@
 status=~/.config/herbstluftwm/status.sh
 
-$status | lemonbar -f "Fantasque Sans Mono:size=13" -f "Ionicons:size=13" -g x30 -B#1a1a1a
+$status | lemonbar -f "Fantasque Sans Mono:size=13" -f "Ionicons:size=13" -g x30 -B#1a1a1a -F#fef8d8
+
