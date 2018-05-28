f09f6170147dd6df59ee15b0d00665c574b22133
Ethan Rooke <ethan@rooke.org>
2018-05-27 19:28:30 -0700

    kill old polybars before spawning new ones

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Remove previously running bars
+killall -q polybar
+
+# Wait for the polybar processes to actually die
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar on each monitor
 for m in $(polybar -m | cut -d ":" -f1);do
 	MONITOR=$m polybar --reload example &
 done
+
+herbstclient -i tag | poly-hlwm-listen &
