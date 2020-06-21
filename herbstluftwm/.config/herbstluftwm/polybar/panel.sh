2d559d1fe43edbfb9fcd85a1a10ca126e7953383
Ethan Rooke <ethan@rooke.org>
2020-06-21 17:21:31 -0500

    Experimenting with theme a little more

--- herbstluftwm/.config/herbstluftwm/polybar/panel.sh
+++ herbstluftwm/.config/herbstluftwm/polybar/panel.sh
@@ -9,7 +9,12 @@ do
 	sleep 1; 
 done
 
+polybar --reload tags &
+polybar --reload info &
+polybar --reload time &
+
+# In case I ever have a more dynamic setup
 # Launch the bar on each monitor
-for m in $(polybar -m | cut -d ":" -f1);do
-	MONITOR=$m polybar --reload example &
-done
+#for m in $(polybar -m | cut -d ":" -f1);do
+	#MONITOR=$m polybar --reload example &
+#done
