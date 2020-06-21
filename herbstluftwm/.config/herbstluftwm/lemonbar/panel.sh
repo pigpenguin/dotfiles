d71eebacb2af167aa6ca0a1eb404f2421b11e0c3
Ethan Rooke <ethan@rooke.org>
2020-06-21 17:21:54 -0500

    I will probably never come back to lemon bar

--- herbstluftwm/.config/herbstluftwm/lemonbar/panel.sh
+++
@@ -1,4 +0,0 @@
-status=~/.config/herbstluftwm/status.sh
-
-$status | lemonbar -f "Fantasque Sans Mono:size=13" -f "Ionicons:size=13" -g x30 -B#1a1a1a -F#fef8d8 | while read line; do eval "$line"; done
-
